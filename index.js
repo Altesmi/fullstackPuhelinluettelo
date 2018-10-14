40c85e73736bb6c8f320551f9e35521928bbe0d7
Olli-Pekka Tikkanen <optikkanen@gmail.com>
2018-10-14 19:49:41 +0300

    Tehtävä 3.13

--- index.js
+++ index.js
@@ -1,94 +1,97 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-const morgan = require('morgan');
-const cors = require('cors');
+const morgan = require("morgan");
+const cors = require("cors");
+const Person = require("./models/person");
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static('build'))
-
+app.use(express.static("build"));
 
 /* define a token function for morgan
 returns the content of the message if any exists */
 
-morgan.token('content', (req,res) => {
-  return JSON.stringify(req.body)
-}
-)
+morgan.token("content", (req, res) => {
+  return JSON.stringify(req.body);
+});
 // use morgan, basically the same setup as 'tiny' but with content added
-app.use(morgan(':method :url :content :status :res[content-length] - :response-time ms'));
-let persons = [
-  {
-    name: "Arto Hellas",
-    number: "040-123456",
-    id: 1
-  },
-  {
-    name: "Martti Tienari",
-    number: "040-123456",
-    id: 2
-  },
-  {
-    name: "Arto Järvinen",
-    number: "040-123456",
-    id: 3
-  },
-  {
-    name: "Lea Kutvonen",
-    number: "040-123456",
-    id: 4
-  }
-];
+app.use(
+  morgan(
+    ":method :url :content :status :res[content-length] - :response-time ms"
+  )
+);
+
+const formatPerson = person => {
+  return {
+    name: person.name,
+    number: person.number,
+    id: person._id
+  };
+};
 
 app.get("/info", (req, res) => {
-  res.send(`<p>Puhelinluettelossa on ${persons.length} henkilön tiedot</p>
+  Person.find({}).then(persons => {
+    res.send(`<p>Puhelinluettelossa on ${persons.length} henkilön tiedot</p>
   <p>Serverin kello on: ${new Date()}`);
+  });
 });
 
 app.get("/api/persons", (req, res) => {
-  res.json(persons);
+  Person.find({}).then(persons => {
+    res.json(persons.map(formatPerson));
+  });
 });
 
 app.get("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  const person = persons.find(person => person.id === id);
-  if (person) {
-    res.json(person);
-  } else {
-    res.status(404).end();
-  }
+  Person.findById(req.params.id)
+    .then(person => {
+      res.json(formatPerson(person));
+    })
+    .catch(error => {
+      console.log(error);
+      res.status(404).end();
+    });
 });
 
 app.delete("/api/persons/:id", (req, res) => {
-  const id = Number(req.params.id);
-  persons = persons.filter(person => person.id !== id);
-
-  res.status(204).end();
+  Person.findByIdAndDelete(req.params.id)
+    .then(result => {
+      res.status(204).end();
+    })
+    .catch(error => {
+      res.status(400).send({ error: "bad id" });
+    });
 });
 
 app.post("/api/persons", (req, res) => {
   // Check that the request has a name and a number and that
   // the name is not in the persons array
 
-  if (req.body.name === undefined) {
+  const body = req.body;
+  if (body.name === undefined) {
     return res.status(400).json({ error: "Name missing" });
-  } else if (req.body.number === undefined) {
+    
+  } else if (body.number === undefined) {
     return res.status(400).json({ error: "Number missing" });
-  } else if (
-    persons.filter(
-      person => person.name.toLowerCase() === req.body.name.toLowerCase()
-    ).length > 0
-  ) {
-    return res.status(400).json({error: 'Name must be unique'})
+    
   }
+  //Define the person as name and number exists
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  });
 
-  const person = req.body;
-  person.id = Math.ceil(Math.random() * 1000000);
-
-  persons = persons.concat(person);
-
-  res.json(person);
+  //save only if the person does not exists
+  Person.find({ name: {$regex: new RegExp(`^${person.name}$`,'i')}}).then(persons => {
+    if (persons.length > 0) { //Person with same name is in the list
+      return res.status(400).json({ error: "Name must be unique" });
+    } else { // name is unique, save person
+      person.save().then(savedPerson => {
+        res.json(formatPerson(savedPerson));
+      });
+    }
+  });
 });
 
 const PORT = process.env.PORT || 3001;
